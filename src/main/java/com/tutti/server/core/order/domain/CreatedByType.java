ad270cbf1533d35314a5605dee4f183dd1baf4b5
heymeworld <khm6153khm@gmail.com>
2025-03-04 19:36:31 +0900

    feat: Order 관련 엔티티 정의 1. Order 엔티티 정의 2. OrderHistory 엔티티 정의 3. OrderItem 엔티티 정의 4. Store 엔티티 정의 5. SystemProcess 엔티티 정의

---
+++ src/main/java/com/tutti/server/core/order/domain/CreatedByType.java
@@ -0,0 +1,9 @@
+package com.tutti.server.core.order.domain;
+
+public enum CreatedByType {
+
+    MEMBER,
+    ADMIN,
+    SYSTEM_PROCESS
+
+}
---
+++ src/main/java/com/tutti/server/core/support/entity/SystemProcess.java
@@ -0,0 +1,28 @@
+package com.tutti.server.core.support.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "system_process")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SystemProcess extends BaseEntity {
+
+    @Column(length = 50)
+    private String systemName;
+
+    @Column(length = 100)
+    private String description;
+
+    @Builder
+    public SystemProcess(String systemName, String description) {
+        this.systemName = systemName;
+        this.description = description;
+    }
+}
