17ef08794b2547e6447a6b82063be946fd49cc8e
greenyeast <djaks65@gmail.com>
2025-03-06 13:24:57 +0900

    feat: fix the EOL error

--- src/main/java/com/tutti/server/core/product/domain/Product.java
+++ src/main/java/com/tutti/server/core/product/domain/Product.java
@@ -1,8 +1,17 @@
 package com.tutti.server.core.product.domain;
 
 import com.tutti.server.core.support.entity.BaseEntity;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Comment;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -14,55 +23,56 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Product extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "product_id", nullable = false)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "product_id", nullable = false)
+  private Long id;
 
-    @Column(name = "bot_id", nullable = false, length = 255)
-    @Comment("botId")
-    private String botId;
+  @Column(name = "bot_id", nullable = false, length = 255)
+  @Comment("botId")
+  private String botId;
 
-    @Column(name = "store_id", nullable = false)
-    @Comment("storeId FK")
-    private Long storeId;
+  @Column(name = "store_id", nullable = false)
+  @Comment("storeId FK")
+  private Long storeId;
 
-    @Column(name = "name", nullable = false, length = 255)
-    @Comment("productName")
-    private String name;
+  @Column(name = "name", nullable = false, length = 255)
+  @Comment("productName")
+  private String name;
 
-    @Column(name = "title_url", nullable = false, length = 2083)
-    @Comment("productImgUrl")
-    private String titleUrl;
+  @Column(name = "title_url", nullable = false, length = 2083)
+  @Comment("productImgUrl")
+  private String titleUrl;
 
-    @Column(name = "product_code")
-    private String productCode;
+  @Column(name = "product_code")
+  private String productCode;
 
-    @Column(name = "detail_url", nullable = false, length = 2083)
-    private String detailUrl;
+  @Column(name = "detail_url", nullable = false, length = 2083)
+  private String detailUrl;
 
-    @Column(name = "description", nullable = false, length = 255)
-    private String description;
+  @Column(name = "description", nullable = false, length = 255)
+  private String description;
 
-    @Column(name = "on_sales", nullable = false)
-    private boolean onSales;
+  @Column(name = "on_sales", nullable = false)
+  private boolean onSales;
 
-    @Column(name = "adult_only", nullable = false)
-    private boolean adultOnly = false;
+  @Column(name = "adult_only", nullable = false)
+  private boolean adultOnly = false;
 
-    @Column(name = "like_count", nullable = false)
-    private int likeCount = 0;
+  @Column(name = "like_count", nullable = false)
+  private int likeCount = 0;
 
-    // 상품명만 필수로 설정하는 빌더
-    @Builder
-    public Product(String botId, Long storeId, String name, String titleUrl, String detailUrl, String categoryId, boolean onSales) {
-        this.botId = botId;
-        this.storeId = storeId;
-        this.name = name;
-        this.titleUrl = titleUrl;
-        this.detailUrl = detailUrl;
-        this.onSales = onSales;
-        this.adultOnly = false;
-        this.likeCount = 0;
-    }
+  // 상품명만 필수로 설정하는 빌더
+  @Builder
+  public Product(String botId, Long storeId, String name, String titleUrl, String detailUrl,
+      String categoryId, boolean onSales) {
+    this.botId = botId;
+    this.storeId = storeId;
+    this.name = name;
+    this.titleUrl = titleUrl;
+    this.detailUrl = detailUrl;
+    this.onSales = onSales;
+    this.adultOnly = false;
+    this.likeCount = 0;
+  }
 }
--- src/main/java/com/tutti/server/core/product/domain/ProductItem.java
+++ src/main/java/com/tutti/server/core/product/domain/ProductItem.java
@@ -1,10 +1,22 @@
 package com.tutti.server.core.product.domain;
 
 import com.tutti.server.core.support.entity.BaseEntity;
-import jakarta.persistence.*;
-import lombok.*;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Comment;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Table(name = "product_items")
@@ -13,32 +25,32 @@ import org.hibernate.annotations.Comment;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ProductItem extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "product_item_id", nullable = false)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "product_item_id", nullable = false)
+  private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "product_id", nullable = false)
-    private Product product;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "product_id", nullable = false)
+  private Product product;
 
-    @Column(name = "original_price")
-    private Integer originalPrice;
+  @Column(name = "original_price")
+  private Integer originalPrice;
 
-    @Column(name = "selling_price", nullable = false)
-    private Integer sellingPrice;
+  @Column(name = "selling_price", nullable = false)
+  private Integer sellingPrice;
 
-    @Column(name = "options", columnDefinition = "TEXT")
-    @Comment("색상=Red, 사이즈=S")
-    private String options;
+  @Column(name = "options", columnDefinition = "TEXT")
+  @Comment("색상=Red, 사이즈=S")
+  private String options;
 
-    // 필수 필드만 포함한 빌더 패턴
-    @Builder
-    public ProductItem(Product product, Integer originalPrice, Integer sellingPrice, String options) {
-        this.product = product;
-        this.originalPrice = originalPrice;
-        this.sellingPrice = sellingPrice;
-        this.options = options;
+  // 필수 필드만 포함한 빌더 패턴
+  @Builder
+  public ProductItem(Product product, Integer originalPrice, Integer sellingPrice, String options) {
+    this.product = product;
+    this.originalPrice = originalPrice;
+    this.sellingPrice = sellingPrice;
+    this.options = options;
 
-    }
+  }
 }
