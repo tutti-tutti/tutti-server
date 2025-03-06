c0877d8bdb000554feccaec8729f2cdb720bda8c
greenyeast <djaks65@gmail.com>
2025-03-06 16:31:03 +0900

    feat: Create entities related to category

--- src/main/java/com/tutti/server/core/product/domain/ProductCategory.java
+++ src/main/java/com/tutti/server/core/product/domain/ProductCategory.java
@@ -1,6 +1,14 @@
 package com.tutti.server.core.product.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,18 +18,19 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ProductCategory {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "category_id")
-    private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "parent_id")
-    private ProductCategory parentCategory;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "category_id")
+  private Long id;
 
-    @Column(name = "name", nullable = false)
-    private String name;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "parent_id")
+  private ProductCategory parentCategory;
 
-    @Column(name = "sort_order", nullable = false)
-    private int sortOrder;
+  @Column(name = "name", nullable = false)
+  private String name;
+
+  @Column(name = "sort_order", nullable = false)
+  private int sortOrder;
 }
--- src/main/java/com/tutti/server/core/product/domain/ProductCategoryMap.java
+++ src/main/java/com/tutti/server/core/product/domain/ProductCategoryMap.java
@@ -1,7 +1,20 @@
 package com.tutti.server.core.product.domain;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "product_category_maps")
@@ -10,31 +23,32 @@ import lombok.*;
 @IdClass(ProductCategoryMap.ProductCategoryMapId.class)
 public class ProductCategoryMap {
 
-    @Id
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "category_id", nullable = false)
-    private ProductCategory category;
+  @Id
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "category_id", nullable = false)
+  private ProductCategory category;
 
-    @Id
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "product_id", nullable = false)
-    private Product product;
+  @Id
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "product_id", nullable = false)
+  private Product product;
 
-    @Column(name = "delete_status", nullable = false)
-    private boolean deleteStatus = false;
+  @Column(name = "delete_status", nullable = false)
+  private boolean deleteStatus;
 
-    @Builder
-    public ProductCategoryMap(ProductCategory category, Product product, boolean deleteStatus) {
-        this.category = category;
-        this.product = product;
-        this.deleteStatus = deleteStatus;
-    }
+  @Builder
+  public ProductCategoryMap(ProductCategory category, Product product, boolean deleteStatus) {
+    this.category = category;
+    this.product = product;
+    this.deleteStatus = deleteStatus;
+  }
 
-    @NoArgsConstructor
-    @AllArgsConstructor
-    @EqualsAndHashCode
-    public static class ProductCategoryMapId implements java.io.Serializable {
-        private Long category;
-        private Long product;
-    }
+  @NoArgsConstructor
+  @AllArgsConstructor
+  @EqualsAndHashCode
+  public static class ProductCategoryMapId implements Serializable {
+
+    private Long category;
+    private Long product;
+  }
 }
