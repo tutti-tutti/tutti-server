f2a0bd2a64a436dfae2662ac2a1cbed032987e3a
Annie <ajm010402@gmail.com>
2025-03-06 16:28:59 +0900

    feat: Member 관련 탈퇴 이력 엔티티 추가

---
+++ src/main/java/com/tutti/server/core/member/domain/WithdrawalHistory.java
@@ -0,0 +1,42 @@
+package com.tutti.server.core.member.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.*;
+
+@Entity
+@Table(name = "withdrawal_histories")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class WithdrawalHistory {
+
+    @Id
+    private Long memberId; // PK이면서 FK 역할
+
+    @OneToOne
+    @MapsId  // member_id를 Member의 PK와 공유
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(columnDefinition = "TEXT")
+    private String reason;
+
+    @Column(length = 1)
+    private String gender; // 'M' 또는 'F'
+
+    @Column
+    private int age; //int는 not null 자동 적용
+
+    @Column(nullable = false)
+    private LocalDateTime withdrawalAt;
+
+    @Builder
+    public WithdrawalHistory(Member member, String reason, String gender, int age, LocalDateTime withdrawalAt) {
+        this.member = member;
+        this.memberId = member.getId(); // member의 PK 값을 가져와서 PK+FK 설정
+        this.reason = reason;
+        this.gender = gender;
+        this.age = age;
+        this.withdrawalAt = withdrawalAt;
+    }
+}
