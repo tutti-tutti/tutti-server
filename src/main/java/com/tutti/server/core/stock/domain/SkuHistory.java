ea64bbe37bc30f9f43bc1edc270a29c02a961527
greenyeast <djaks65@gmail.com>
2025-03-07 02:03:17 +0900

    feat: Create entity related stock

---
+++ src/main/java/com/tutti/server/core/stock/domain/SkuHistory.java
@@ -0,0 +1,39 @@
+package com.tutti.server.core.stock.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "sku_histories")
+@Getter
+@NoArgsConstructor
+public class SkuHistory {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "sku_history_id")
+  private Long skuHistoryId;
+
+  @Column(name = "type", nullable = false)
+  private Byte type;
+
+  @Column(name = "quantity", nullable = false)
+  private int quantity;
+
+  @Column(name = "created_at", nullable = false, updatable = false)
+  private LocalDateTime createdAt;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "sku_code", nullable = false)
+  private Sku sku;
+}
