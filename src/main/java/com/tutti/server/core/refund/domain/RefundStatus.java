ce61861c6127dc0d9033dc0c6ce3528a33282202
ShinDongJun <tlsehdwns147@naver.com>
2025-03-07 16:05:35 +0900

    feat: 환불 관엔티티 수정

--- src/main/java/com/tutti/server/core/refund/domain/RefundStatus.java
+++ src/main/java/com/tutti/server/core/refund/domain/RefundStatus.java
@@ -3,6 +3,5 @@ package com.tutti.server.core.refund.domain;
 public enum RefundStatus {
 
     REFUND_REQUESTED,    // 환불 요청
-    REFUND_PROCESSING,   // 환불 처리중
     REFUND_COMPLETED;    // 환불 완료
 }
