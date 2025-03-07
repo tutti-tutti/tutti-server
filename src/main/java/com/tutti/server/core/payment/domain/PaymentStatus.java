ca8b2d50a69978aebda566ed4aa4f2afb724c88b
ShinDongJun <tlsehdwns147@naver.com>
2025-03-07 14:34:45 +0900

    feat: 결제 관련 엔티티 수정 1. ãPaymentStatus 수정

--- src/main/java/com/tutti/server/core/payment/domain/PaymentStatus.java
+++ src/main/java/com/tutti/server/core/payment/domain/PaymentStatus.java
@@ -2,7 +2,6 @@ package com.tutti.server.core.payment.domain;
 
 public enum PaymentStatus {
 
-    PENDING,        // 결제 대기 (결제 요청 전)
     REQUESTED,      // 결제 요청 (PG사에 결제 요청)
     APPROVED,       // 결제 승인 (PG사에서 결제 완료 응답)
     FAILED,         // 결제 실패 (PG사 결제 실패 응답)
