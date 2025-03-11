a76ba485e1a220848b360d9b782e968afbd87199
leeboseok <boseok.lee@hotmail.com>
2025-03-11 22:19:50 +0900

    Feat: 리뷰 수정 1. ãFaqSearchRequest ã레코드 생성 2. FaqListReponse 변수 이름 변경 3. 컨í

--- src/main/java/com/tutti/server/core/faq/payload/response/FaqListResponse.java
+++ src/main/java/com/tutti/server/core/faq/payload/response/FaqListResponse.java
@@ -5,14 +5,10 @@ import java.util.List;
 
 @Schema(description = "FAQ 목록 응답")
 public record FaqListResponse(
-    @Schema(description = "총 결과 개수", example = "9") int totalResults,
+    @Schema(description = "총 결과 개수", example = "9") int totalFaq,
     @Schema(description = "현재 페이지 번호", example = "1") int currentPage,
     @Schema(description = "페이지당 FAQ 개수", example = "10") int size,
     @Schema(description = "FAQ 목록") List<FaqResponse> results
 ) {
 
-    public static FaqListResponse from(int totalResults, int currentPage, int size,
-        List<FaqResponse> results) {
-        return new FaqListResponse(totalResults, currentPage, size, results);
-    }
 }
