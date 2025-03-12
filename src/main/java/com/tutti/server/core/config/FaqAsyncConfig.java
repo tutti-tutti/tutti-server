aec84767c4c14ff8a8c8b1a0cb07acfaf289a8dd
leeboseok <boseok.lee@hotmail.com>
2025-03-12 12:28:17 +0900

    Feat: Faq ì단일 조회 기능 구현 1. Faq 조회수 비동기 처리 추가 2. FaqRequest 레코드 작성 3. 주석 정리

---
+++ src/main/java/com/tutti/server/core/config/FaqAsyncConfig.java
@@ -0,0 +1,13 @@
+package com.tutti.server.core.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+/**
+ * FAQ 서비스의 비동기 처리 활성화 설정 클래스
+ */
+@Configuration
+@EnableAsync
+public class FaqAsyncConfig {
+
+}
