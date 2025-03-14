9a1dab5f7a11af67363c1f814c113d6b45205e42
leeboseok <boseok.lee@hotmail.com>
2025-03-14 12:37:28 +0900

    Feat: 인터페이스 애너테이션 수정

--- src/main/java/com/tutti/server/core/review/api/ReviewApiSpec.java
+++ src/main/java/com/tutti/server/core/review/api/ReviewApiSpec.java
@@ -1,11 +1,13 @@
 package com.tutti.server.core.review.api;
 
 import com.tutti.server.core.review.payload.request.ReviewCreateRequest;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 
 @Tag(name = "리뷰 문서")
 public interface ReviewApiSpec {
 
+    @Operation(summary = "리뷰 작성 API")
     ResponseEntity<String> createReview(ReviewCreateRequest reviewCreateRequest);
 }
