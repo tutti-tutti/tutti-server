444ff2122ca9919645d5843c96f6ae666ee9a8b9
leeboseok <boseok.lee@hotmail.com>
2025-03-16 19:50:53 +0900

    Feat: 2차 커밋 1. 레코드 클래스에 NotNull 애너테이션 추가

--- src/main/java/com/tutti/server/core/faq/payload/request/FaqUpdateRequest.java
+++ src/main/java/com/tutti/server/core/faq/payload/request/FaqUpdateRequest.java
@@ -1,22 +1,28 @@
 package com.tutti.server.core.faq.payload.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 
 @Schema(description = "FAQ 수정 요청")
 public record FaqUpdateRequest(
 
+    @NotNull(message = "FAQ 카테고리 ID를 입력해주세요.")
     @Schema(description = "FAQ 카테고리")
     long categoryId,
 
+    @NotNull(message = "FAQ ID를 입력해주세요.")
     @Schema(description = "FAQ ID", example = "1")
     long faqId,
 
+    @NotNull(message = "FAQ 질문을 입력해주세요.")
     @Schema(description = "FAQ 질문", example = "배송 기간은 얼마나 걸리나요?")
     String question,
 
+    @NotNull(message = "FAQ 답변을 입력해주세요.")
     @Schema(description = "FAQ 답변", example = "일반적으로 3~5일이 소요됩니다.")
     String answer,
 
+    @NotNull(message = "FAQ 노출 여부를 입력해주세요.")
     @Schema(description = "FAQ 노출 여부", example = "true")
     boolean isView
 
