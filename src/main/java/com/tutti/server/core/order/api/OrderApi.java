07ff107575397eb97b0b0224d4ae5d06780ccd6b
heymeworld <com01@knou.ac.kr>
2025-03-17 01:01:59 +0900

    feat: 주문 생성 기능 구현 >> 1. 유저는 상품을 주문할 수 있다. >> 2. Order request: member의 인증 정보(회원일 경우), 결제수단, 주문 상품 >> 3. 나머지는 직접 생성: 주문 상태, 주문 번호, 주문 상품 수, 주문 금액 >> 4. 배송비는 우선 아예 안 받는 것으로 한다. (무료배송) >> 5. OrderItem request: member의 인증 정보(회원일 경우), product_item_id(상품명, 상품사진, 옵션, 가격), 수량 >> 6. 주문 엔티티 생성과 함께 주문 이력 엔티티도 생성된다.

---
+++ src/main/java/com/tutti/server/core/order/api/OrderApi.java
@@ -0,0 +1,24 @@
+package com.tutti.server.core.order.api;
+
+import com.tutti.server.core.order.application.OrderService;
+import com.tutti.server.core.order.payload.request.OrderCreateRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/orders")
+public class OrderApi implements OrderApiSpec {
+
+    private final OrderService orderService;
+
+    @Override
+    @PostMapping
+    public void createOrder(@RequestBody OrderCreateRequest request) {
+        orderService.createOrder(request);
+    }
+
+}
