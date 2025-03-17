e897bd476ee9fbe7138afad098e517d98cb3ff44
greenyeast <djaks65@gmail.com>
2025-03-17 13:59:52 +0900

    feat: dummy data for product in eletronic device

--- src/main/java/com/tutti/server/core/product/domain/Product.java
+++ src/main/java/com/tutti/server/core/product/domain/Product.java
@@ -6,7 +6,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
@@ -24,70 +24,69 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Product extends BaseEntity {
 
-  @Column(name = "bot_id")
-  @NotNull
-  @Comment("botId")
-  private String botId;
+    @Column(name = "bot_id")
+    @NotNull
+    @Comment("botId")
+    private String botId;
 
-  @OneToOne
-  @JoinColumn(name = "store_id")
-  @NotNull
-  @Comment("storeId")
-  private Store storeId;
+    @ManyToOne
+    @JoinColumn(name = "store_id")
+    @NotNull
+    @Comment("storeId")
+    private Store storeId;
 
-  @Column(name = "name")
-  @NotNull
-  @Comment("productName")
-  private String name;
+    @Column(name = "name")
+    @NotNull
+    @Comment("productName")
+    private String name;
 
-  @Column(name = "title_url", nullable = false, length = 2083)
-  @NotNull
-  @Comment("productImgUrl")
-  private String titleUrl;
+    @Column(name = "title_url", length = 2083)
+    @NotNull
+    @Comment("productImgUrl")
+    private String titleUrl;
 
-  @Column(name = "product_code")
-  @NotNull
-  @Comment("productCode")
-  private String productCode;
+    @Column(name = "product_code")
+    @NotNull
+    @Comment("productCode")
+    private String productCode;
 
-  @Column(name = "detail_url", length = 2083)
-  @Comment("이후추가")
-  private String detailUrl;
+    @Column(name = "detail_url", length = 2083)
+    @Comment("이후추가")
+    private String detailUrl;
 
-  @Column(name = "description")
-  @NotNull
-  private String description;
+    @Column(name = "description")
+    private String description;
 
-  @Column(name = "on_sales")
-  @NotNull
-  private boolean onSales;
+    @Column(name = "on_sales")
+    @NotNull
+    private boolean onSales;
 
-  @Column(name = "adult_only")
-  @NotNull
-  private boolean adultOnly;
+    @Column(name = "adult_only")
+    @NotNull
+    private boolean adultOnly;
 
-  @Column(name = "max_quantity")
-  @NotNull
-  private int maxQuantity;
+    @Column(name = "max_quantity")
+    @NotNull
+    private int maxQuantity;
 
-  @Column(name = "like_count")
-  @NotNull
-  private int likeCount;
+    @Column(name = "like_count")
+    @NotNull
+    private int likeCount;
 
-  // 필수로 설정하는 빌더
-  @Builder
-  public Product(String botId, Store storeId, String name, String titleUrl, String productCode,
-      String detailUrl, String description, boolean onSales, boolean adultOnly,
-      int likeCount) {
-    this.botId = botId;
-    this.storeId = storeId;
-    this.name = name;
-    this.titleUrl = titleUrl;
-    this.productCode = productCode;
-    this.detailUrl = detailUrl;
-    this.description = description;
-    this.onSales = onSales;
-    this.adultOnly = adultOnly;
-    this.likeCount = likeCount;
-  }
+    // 필수로 설정하는 빌더
+    @Builder
+    public Product(String botId, Store storeId, String name, String titleUrl, String productCode,
+            String detailUrl, String description, boolean onSales, boolean adultOnly,
+            int likeCount) {
+        this.botId = botId;
+        this.storeId = storeId;
+        this.name = name;
+        this.titleUrl = titleUrl;
+        this.productCode = productCode;
+        this.detailUrl = detailUrl;
+        this.description = description;
+        this.onSales = onSales;
+        this.adultOnly = adultOnly;
+        this.likeCount = likeCount;
+    }
 }
--- src/main/java/com/tutti/server/core/product/domain/ProductItem.java
+++ src/main/java/com/tutti/server/core/product/domain/ProductItem.java
@@ -1,6 +1,10 @@
 package com.tutti.server.core.product.domain;
 
+import org.hibernate.annotations.Comment;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import com.tutti.server.core.support.entity.BaseEntity;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
@@ -13,8 +17,6 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.Comment;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Table(name = "product_items")
@@ -23,39 +25,39 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ProductItem extends BaseEntity {
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "product_id")
-  @NotNull
-  private Product product;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    @NotNull
+    private Product product;
 
-  @Column(name = "original_price")
-  private Integer originalPrice;
+    @Column(name = "original_price")
+    private Integer originalPrice;
 
-  @Column(name = "selling_price", nullable = false)
-  @NotNull
-  private Integer sellingPrice;
+    @Column(name = "selling_price")
+    @NotNull
+    private Integer sellingPrice;
 
-  @Column(name = "option_name", columnDefinition = "TEXT")
-  @Comment("색상, 사이즈")
-  private String optionName;
+    @Column(name = "option_name", columnDefinition = "TEXT")
+    @Comment("색상, 사이즈")
+    private String optionName;
 
-  @Column(name = "option_value", columnDefinition = "TEXT")
-  @Comment("Red, S")
-  private String optionValue;
+    @Column(name = "option_value", columnDefinition = "TEXT")
+    @Comment("Red, S")
+    private String optionValue;
 
-  @Column(name = "soldout")
-  @NotNull
-  private boolean soldOut;
+    @Column(name = "sold_out")
+    @NotNull
+    private boolean soldOut;
 
-  // 필수 필드만 포함한 빌더 패턴
-  @Builder
-  public ProductItem(Product product, Integer originalPrice, Integer sellingPrice,
-      String optionName, String optionValue, boolean soldOut) {
-    this.product = product;
-    this.originalPrice = originalPrice;
-    this.sellingPrice = sellingPrice;
-    this.optionName = optionName;
-    this.optionValue = optionValue;
-    this.soldOut = soldOut;
-  }
+    // 필수 필드만 포함한 빌더 패턴
+    @Builder
+    public ProductItem(Product product, Integer originalPrice, Integer sellingPrice,
+            String optionName, String optionValue, boolean soldOut) {
+        this.product = product;
+        this.originalPrice = originalPrice;
+        this.sellingPrice = sellingPrice;
+        this.optionName = optionName;
+        this.optionValue = optionValue;
+        this.soldOut = soldOut;
+    }
 }
