9512d74827501fa583c78e713c9ee18e398fe2f6
leeboseok <boseok.lee@hotmail.com>
2025-03-17 13:53:03 +0900

    Refactor: FaqServiceImple 예외처리 제거

--- src/main/java/com/tutti/server/core/faq/application/user/FaqServiceImpl.java
+++ src/main/java/com/tutti/server/core/faq/application/user/FaqServiceImpl.java
@@ -5,8 +5,6 @@ import com.tutti.server.core.faq.payload.request.FaqListRequest;
 import com.tutti.server.core.faq.payload.request.FaqSearchRequest;
 import com.tutti.server.core.faq.payload.response.FaqListResponse;
 import com.tutti.server.core.faq.payload.response.FaqResponse;
-import com.tutti.server.core.support.exception.DomainException;
-import com.tutti.server.core.support.exception.ExceptionType;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -23,50 +21,27 @@ public class FaqServiceImpl implements FaqService {
     private final FaqFeedbackService faqFeedbackService;
 
     public List<String> getCategories() {
-        try {
-            return faqCategoryListService.getCategories();
-        } catch (Exception e) {
-            throw new DomainException(ExceptionType.FAQ_CATEGORY_NOT_FOUND);
-        }
+        return faqCategoryListService.getCategories();
     }
 
     public FaqListResponse getFaqs(FaqListRequest request) {
-        try {
-            return faqListViewService.getFaqs(request);
-        } catch (Exception e) {
-            throw new DomainException(ExceptionType.FAQ_NOT_FOUND);
-        }
+        return faqListViewService.getFaqs(request);
     }
 
     public FaqListResponse searchFaqs(FaqSearchRequest request) {
-        try {
-            return faqSearchListService.searchFaqs(request);
-        } catch (Exception e) {
-            throw new DomainException(ExceptionType.FAQ_NOT_FOUND);
-        }
+        return faqSearchListService.searchFaqs(request);
     }
 
     public List<FaqResponse> getTopFaqs(int limit) {
-        try {
-            return faqTopViewedListService.getTopFaqs(limit);
-        } catch (Exception e) {
-            throw new DomainException(ExceptionType.FAQ_NOT_FOUND);
-        }
+        return faqTopViewedListService.getTopFaqs(limit);
     }
 
     public FaqResponse getFaqById(Long faqId) {
-        try {
-            return faqViewDetailService.findFaqById(faqId);
-        } catch (Exception e) {
-            throw new DomainException(ExceptionType.FAQ_NOT_FOUND);
-        }
+        return faqViewDetailService.findFaqById(faqId);
     }
 
     public void faqFeedback(Long faqId, FaqFeedbackRequest feedback) {
-        try {
-            faqFeedbackService.updateFaqFeedback(faqId, feedback);
-        } catch (Exception e) {
-            throw new DomainException(ExceptionType.FAQ_FEEDBACK_FAILED);
-        }
+        faqFeedbackService.updateFaqFeedback(faqId, feedback);
     }
 }
+
