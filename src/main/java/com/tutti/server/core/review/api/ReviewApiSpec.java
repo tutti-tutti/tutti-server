54717e7c8f6f2050cc28deed501c7f292dc9ecf8
leeboseok <boseok.lee@hotmail.com>
2025-03-17 20:53:24 +0900

    [BE] DIP 원칙 개선 1. 인터페이스에서 앤드포인트 정의 2. 컨트롤러에서 앤드포인트 매핑

--- src/main/java/com/tutti/server/core/review/api/ReviewApiSpec.java
+++ src/main/java/com/tutti/server/core/review/api/ReviewApiSpec.java
@@ -6,13 +6,21 @@ import com.tutti.server.core.review.payload.response.ReviewListResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Tag(name = "리뷰 문서")
+@RequestMapping("/reviews")
 public interface ReviewApiSpec {
 
     @Operation(summary = "리뷰 작성 API")
-    ResponseEntity<String> createReview(ReviewCreateRequest reviewCreateRequest);
+    @PostMapping
+    ResponseEntity<String> createReview(
+        @RequestBody ReviewCreateRequest reviewCreateRequest);
 
     @Operation(summary = "리뷰 목록 조회 API")
+    @GetMapping("/{productId}")
     ResponseEntity<ReviewListResponse> getReviewList(ReviewListRequest reviewListRequest);
 }
