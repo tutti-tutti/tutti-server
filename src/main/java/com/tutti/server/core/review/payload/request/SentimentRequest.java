7c9001b828bd239ffd5a21969fa33ca32e9bca56
leeboseok <boseok.lee@hotmail.com>
2025-03-17 15:49:36 +0900

    Feat: 레코드 클레스에 애너테이션 추가

--- src/main/java/com/tutti/server/core/review/payload/request/SentimentRequest.java
+++ src/main/java/com/tutti/server/core/review/payload/request/SentimentRequest.java
@@ -1,6 +1,10 @@
 package com.tutti.server.core.review.payload.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "감정 분석 요청 파라미터")
 public record SentimentRequest(
+    @Schema(description = "리뷰 내용", example = "울 신랑 출근할때 쓰라고 사줬어요. 좋다고 하네요.")
     String review_text
 ) {
 
