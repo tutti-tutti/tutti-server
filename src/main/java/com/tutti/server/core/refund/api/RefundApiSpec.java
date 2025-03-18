d6df49913d2dd24d839f1bdc045e4c9af9fc9158
ShinDongJun <tlsehdwns147@naver.com>
2025-03-18 16:06:56 +0900

    feat: file changes 감소

--- src/main/java/com/tutti/server/core/refund/api/RefundApiSpec.java
+++ src/main/java/com/tutti/server/core/refund/api/RefundApiSpec.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 @Tag(name = "환불 API", description = "환불 요청을 처리하는 API")
 public interface RefundApiSpec {
 
-    @Operation(summary = "환불 요청", description = "사용자가 환불을 요청하는 API")
+    @Operation(summary = "환불 요청", description = "사용자가 환불을 요청하는 API(환불 요청시 결제취소 프로세스도 실행)")
     ResponseEntity<Map<String, String>> requestRefund(RefundRequest request);
-    
+
 }
