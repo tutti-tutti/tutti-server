b7e920a09d60ccb984c3359772579b957a8adacd
heymeworld <khm6153khm@gmail.com>
2025-03-20 14:00:29 +0900

    refactor: 장바구니 조회 기능 수정 1. 장바구니 엔티티 필드 추가 2. 장바구니 조회 응답 DTO 필드 추가

--- src/main/java/com/tutti/server/core/cart/payload/request/CartItemCreateRequest.java
+++ src/main/java/com/tutti/server/core/cart/payload/request/CartItemCreateRequest.java
@@ -3,6 +3,7 @@ package com.tutti.server.core.cart.payload.request;
 import com.tutti.server.core.cart.domain.CartItem;
 import com.tutti.server.core.member.domain.Member;
 import com.tutti.server.core.product.domain.ProductItem;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
@@ -11,11 +12,12 @@ import lombok.Builder;
 public record CartItemCreateRequest(
 
         Long memberId,
-        
+
         @NotNull(message = "필수 옵션을 선택해주세요.")
         Long productItemId,
 
         @Min(value = 1, message = "수량은 1 이상이어야 합니다.")
+        @Max(value = 10, message = "최대 10개까지 주문 가능합니다.")
         int quantity
 
 ) {
