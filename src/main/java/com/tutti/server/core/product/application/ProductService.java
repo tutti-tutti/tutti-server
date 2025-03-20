a69d9e558cf0348eecedd8aa2df3c79c281c159c
greenyeast <djaks65@gmail.com>
2025-03-20 20:59:56 +0900

    feat: second feedback

--- src/main/java/com/tutti/server/core/product/application/ProductService.java
+++ src/main/java/com/tutti/server/core/product/application/ProductService.java
@@ -2,11 +2,10 @@ package com.tutti.server.core.product.application;
 
 import com.tutti.server.core.product.payload.response.ProductResponse;
 import java.util.List;
-import org.springframework.web.bind.annotation.PathVariable;
 
 public interface ProductService {
 
-    public List<ProductResponse> getAllProductsByCategory(@PathVariable Long categoryId);
+    public List<ProductResponse> getAllProductsByCategory(Long categoryId);
 
     public List<ProductResponse> getAllProductsByCreated();
 }
