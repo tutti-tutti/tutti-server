db93b0f7e08f17c8149c9e2758c887dabe8a5dc4
ShinDongJun <tlsehdwns147@naver.com>
2025-03-21 12:51:07 +0900

    feat: 오타 수정

--- src/main/java/com/tutti/server/core/refund/api/RefundApiSpec.java
+++ src/main/java/com/tutti/server/core/refund/api/RefundApiSpec.java
@@ -12,7 +12,7 @@ public interface RefundApiSpec {
     @Operation(summary = "환불 요청", description = "사용자가 환불을 요청하는 API(환불 요청시 결제취소 프로세스도 실행)")
     void requestRefund(PaymentCancelRequest request);
 
-    @Operation(summary = "주분번호로 환불 조회", description = "사용자가 환불의 진행상태를 조회하기 위한 API")
+    @Operation(summary = "주문번호로 환불 조회", description = "사용자가 환불의 진행상태를 조회하기 위한 API")
     RefundViewResponse getRefundViewByOrderId(Long orderId,
             CustomUserDetails userDetails);
 
