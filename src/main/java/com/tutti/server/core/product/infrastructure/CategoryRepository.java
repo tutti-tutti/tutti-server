c88c7e5e76ca7dd0d5607d9e5e9d8f9af09b7459
greenyeast <djaks65@gmail.com>
2025-03-21 18:51:24 +0900

    feat: fix productCategory class name

--- src/main/java/com/tutti/server/core/product/infrastructure/CategoryRepository.java
+++ src/main/java/com/tutti/server/core/product/infrastructure/CategoryRepository.java
@@ -1,20 +1,20 @@
 package com.tutti.server.core.product.infrastructure;
 
+import com.tutti.server.core.product.domain.ProductCategory;
 import com.tutti.server.core.support.exception.DomainException;
 import com.tutti.server.core.support.exception.ExceptionType;
 import java.util.List;
-import java.util.Locale.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface CategoryRepository extends JpaRepository<Category, Long> {
+public interface CategoryRepository extends JpaRepository<ProductCategory, Long> {
 
-    default Category findOne(Long id) {
+    default ProductCategory findOne(Long id) {
         return findById(id)
                 .orElseThrow(() -> new DomainException(ExceptionType.CATEGORY_NOT_FOUND));
     }
 
     @Query("SELECT pc FROM ProductCategory pc LEFT JOIN FETCH pc.parentCategory ORDER BY pc.sortOrder")
-    List<Category> findAllWithParent();
+    List<ProductCategory> findAllWithParent();
 
 }
