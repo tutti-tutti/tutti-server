9fad267a8706ff229b5fd2aa0adcc642de7c5bb7
greenyeast <djaks65@gmail.com>
2025-03-24 14:50:41 +0900

    feat: fix response

--- src/main/java/com/tutti/server/core/product/payload/response/CategoryResponse.java
+++ src/main/java/com/tutti/server/core/product/payload/response/CategoryResponse.java
@@ -6,19 +6,31 @@ import lombok.Builder;
 
 @Builder
 public record CategoryResponse(
-
         Long id,
         int sortOrder,
         String name,
-        CategoryResponse parentCategory
-
+        ParentCategoryResponse parentCategory
 ) {
-
     public static CategoryResponse fromEntity(ProductCategory productCategory) {
         return CategoryResponse.builder()
                 .id(productCategory.getId())
                 .sortOrder(productCategory.getSortOrder())
                 .name(productCategory.getName())
+                .parentCategory(productCategory.getParentCategory() != null 
+                    ? ParentCategoryResponse.fromEntity(productCategory.getParentCategory()) 
+                    : null)
+                .build();
+    }
+}
+
+@Builder
+record ParentCategoryResponse(
+        Long id,
+        ParentCategoryResponse parentCategory
+) {
+    public static ParentCategoryResponse fromEntity(ProductCategory productCategory) {
+        return ParentCategoryResponse.builder()
+                .id(productCategory.getId())
                 .parentCategory(productCategory.getParentCategory() != null 
                     ? fromEntity(productCategory.getParentCategory()) 
                     : null)
