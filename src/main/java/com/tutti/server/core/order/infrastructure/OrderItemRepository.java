7db859b2ea35662bdb14cd44e00fc7dd83451678
leeboseok <boseok.lee@hotmail.com>
2025-03-25 21:45:27 +0900

    [BE] Feat: 요청값에 주문ID 추가

--- src/main/java/com/tutti/server/core/order/infrastructure/OrderItemRepository.java
+++ src/main/java/com/tutti/server/core/order/infrastructure/OrderItemRepository.java
@@ -6,6 +6,7 @@ import com.tutti.server.core.support.exception.ExceptionType;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 
@@ -16,7 +17,17 @@ public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 
     List<OrderItem> findAllByOrderId(Long orderId);
 
-    @Query("SELECT COUNT(oi) > 0 FROM OrderItem oi " +
-            "WHERE oi.order.member.id = :memberId AND oi.productItem.id = :productItemId")
-    boolean existsByOrderMemberIdAndProductItemId(Long memberId, Long productItemId);
+    @Query("""
+                SELECT COUNT(oi) > 0 
+                FROM OrderItem oi 
+                WHERE oi.order.id = :orderId 
+                  AND oi.order.member.id = :memberId 
+                  AND oi.productItem.id = :productItemId
+            """)
+    boolean existsByOrderIdAndOrderMemberIdAndProductItemId(
+            @Param("orderId") Long orderId,
+            @Param("memberId") Long memberId,
+            @Param("productItemId") Long productItemId
+    );
+
 }
